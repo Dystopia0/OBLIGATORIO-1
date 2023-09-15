6af840e2e69f4891e7658656d0d202a2d52218fb
Dystopia0 <fernando.sobrera@icloud.com>
2023-09-14 21:09:37 -0300

    jj

--- src/directorio.cpp
+++ src/directorio.cpp
@@ -11,6 +11,34 @@ struct _rep_directorio {
    _rep_directorio* raiz;
    _rep_directorio* sig;
    _rep_directorio* ant;
-    *TArchivo listaArchivo;
+   TArchivo *listaArchivo;
 };
 typedef struct _rep_directorio *TDirectorio;
+
+//retorna true si el directorio "directorio" no tiene archivos
+bool isEmptyDirectory(TDirectorio directorio)
+{
+    return directorio == NULL;
+}
+
+
+//pre-condicion existe el archivo de nombre nombreArchivo en el directorio "directorio"
+//pos-condicion: retorna un puntero al archivo de nombre "nombreArchivo"
+TArchivo getFileDirectory(TDirectorio directorio, Cadena nombreArchivo)
+{
+
+}
+
+//pre condicion: el archivo nombreArchivo existe en directorio
+//pos-condicion: inserta una nueva fila al comienzo del archivo nombreArchivo conteniendo los chars texto
+void insertTextFile(TDirectorio& directorio, Cadena nombreArchivo, Cadena texto)
+{
+
+}
+
+//pre-condicion: existe el archivo de nombre "nombreArchivo" en el directorio "directorio"
+//pos-condicion: elimina el archivo del directorio "directorio" y toda la memoria utilizada por este.
+void deleteFileDirectory(TDirectorio& directorio, Cadena nombreArchivo)
+{
+
+}
